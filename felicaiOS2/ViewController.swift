c2da73d9d51dcb536579a141c7a5859fe34794ab
YutaMatsumura <yuta.matsumura@wasedasai.net>
2021-08-12 20:44:32 +0900

    Initial Commit

---
+++ felicaiOS2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  felicaiOS2
+//
+//  Created by 松村悠太 on 2021/08/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
